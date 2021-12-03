e99864bbaea13b5eb3c368117e5f17f3f8bb039e
novaspark <xiayanzheng@outlook.com>
2021-12-03 22:59:02 +0800

    modify pg docker sh

--- install_postgresql_docker.sh
+++ install_postgresql_docker.sh
@@ -2,7 +2,7 @@ hostname=$(hostname | tr "[:upper:]" "[:lower:]")
 postgres_container_name=$hostname"_"postgresql
 docker run -d \
     --name $postgres_container_name \
-    -e POSTGRES_PASSWORD=mysecretpassword \
+    -e POSTGRES_PASSWORD=$* \
     -e PGDATA=/var/lib/postgresql/data/pgdata \
     -v /custom/mount:/var/lib/postgresql/data \
     postgres
