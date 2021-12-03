0c2b785ade37813a82d58c0be678fd5034600ac8
novaspark <xiayanzheng@outlook.com>
2021-12-03 22:37:23 +0800

    fix postgre docker

--- install_postgresql_docker.sh
+++ install_postgresql_docker.sh
@@ -1,9 +1,9 @@
 hostname=$(hostname | tr "[:upper:]" "[:lower:]")
 postgres_img_name=$hostname"_"postgresql
 docker run -d \
-    --name some-postgres \
+    --name $postgres_img_name \
     -e POSTGRES_PASSWORD=mysecretpassword \
     -e PGDATA=/var/lib/postgresql/data/pgdata \
     -v /custom/mount:/var/lib/postgresql/data \
-    $postgres_img_name
+    postgres
 docker ps
