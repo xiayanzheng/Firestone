bead6631cf38a34c973fbd320e5b16798ae655a4
novaspark <xiayanzheng@outlook.com>
2021-12-04 15:14:31 +0800

    add solution

---
+++ services/install_trojan_go_docker.sh
@@ -0,0 +1,6 @@
+hostname=$(hostname | tr "[:upper:]" "[:lower:]")
+container_name=$hostname"_"trojan
+mkdir -p /etc/lfapps/trojan
+mkdir -p /etc/letsencrypt
+mkdir -p /etc/letsencrypt/live/xiayanzheng.com/
+docker run -d --network host --name $container_name --restart=always -v /etc/lfapps/trojan:/etc/trojan-go -v /etc/letsencrypt:/etc/letsencrypt:rw teddysun/trojan-go
---
+++ solutions/ga_server_v1.sh
@@ -0,0 +1,14 @@
+# install v2ray
+wget https://raw.githubusercontent.com/xiayanzheng/Firestone/master/services/install_v2ray_docker.sh
+chmod u+x install_v2ray_docker.sh
+./install_v2ray_docker.sh
+
+# install xray
+wget https://raw.githubusercontent.com/xiayanzheng/Firestone/master/services/install_xray_docker.sh
+chmod u+x install_xray_docker.sh
+./install_xray_docker.sh
+
+# install node exporter
+wget https://raw.githubusercontent.com/xiayanzheng/Firestone/master/observer/prometheus/install_node_exporter_docker.sh
+chmod u+x install_node_exporter_docker.sh
+./install_node_exporter_docker.sh
