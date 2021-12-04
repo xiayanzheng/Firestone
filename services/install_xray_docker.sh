bd39ae1e1e0b65f6a4d8cde822ecc674de723881
novaspark <xiayanzheng@outlook.com>
2021-12-04 14:54:42 +0800

    fix xray docker

--- services/install_xray_docker.sh
+++ services/install_xray_docker.sh
@@ -1,5 +1,7 @@
 hostname=$(hostname | tr "[:upper:]" "[:lower:]")
 container_name=$hostname"_"xray
 mkdir -p /etc/lfapps/xray
+wget https://raw.githubusercontent.com/xiayanzheng/Firestone/master/services/config_files/xray/config.json
+mv config.json /etc/lfapps/xray/
 docker run -d -p 9006:10086 --name $container_name --restart=always -v /etc/lfapps/xray:/etc/xray teddysun/xray
-cat /etc/lfapps/xrayconfig.json
+cat /etc/lfapps/xray/config.json
